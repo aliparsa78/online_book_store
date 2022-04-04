7ada189bb078d773d47eda76476ca12519f64c69
aliparsa <aliparsa9@gmail.com>
2022-04-04 13:37:40 +0430

    Catagories Table added

---
+++ database/migrations/2022_04_04_085810_create_catagories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatagoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catagories', function (Blueprint $table) {
+            $table->id();
+            // catagory name forexample novel,math,science
+            $table->string('catagory_name');
+            $table->string('discription');
+            $table->char('status');
+            $table->string('popular');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catagories');
+    }
+}
