48f6e18c652c7d693c4c88d92d9b4592c08b164d
aliparsa <aliparsa9@gmail.com>
2022-04-05 21:45:20 +0430

    Font awesome and sidebar dashboard added

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,15 +10,35 @@
     <script src="assets/js/popper.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/back/style.css">
 </head>
 <body>
 @include('../layouts/backend/header')
         <div class="row p-5">
-            <div class="col-md-3 bg-info">
-                <p>dashboard</p>
+            <div class="col-md-3 ">
+                <div class="admin-dashboard">
+                    <a href="">Dashboard</a>
+                    <ul>
+                        <li>
+                            <div class="dropdown">
+                            <a type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                Books <i class="fa fa-book"></i>
+                            </a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Link 1</a>
+                                <a class="dropdown-item" href="#">Link 2</a>
+                                <a class="dropdown-item" href="#">Link 3</a>
+                            </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="">Catagories</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-md-9 bg-success">
+            <div class="col-md-9 ">
                 <div class="row">
                     <div class="col-md-4 col-sm-4">
                         <div class="card">
